8cb505b66e0b7d4dfa7e7754b10934da531b1952
Lev Sivashov <lev@yoxel.com>
2017-01-30 10:43:01 +0200

    Chapter 3. ex. 5, 6

--- chapter3/Chapter3.hs
+++ chapter3/Chapter3.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 -- 1, 2. Write a function that computes the number of elements in a list.
 listLength :: [a] -> Int
 listLength (_ : xs) = 1 + listLength(xs)
@@ -12,11 +14,26 @@ meanValue (xs) = fromIntegral(listSum xs) / fromIntegral(listLength xs)
 -- 4. Turn a list into a palindrome; i.e., it should read the same both backward and forward.
 -- e.g. [1, 2, 3] -> [1, 2, 3, 3, 2, 1]
 toPalindrome :: [a] -> [a]
-toPalindrome xs = append xs (reverse xs [])
-    where reverse [] result = result
-          reverse (x:xs) result = reverse xs (x:result)
-          append (x:xs) ys = x : append xs ys
+toPalindrome xs = append xs (listReverse xs)
+    where append (x:xs) ys = x : append xs ys
           append [] ys = ys
 
+listReverse :: [a] -> [a]
+listReverse xs = doReverse xs []
+    where doReverse [] result = result
+          doReverse (x:xs) result = doReverse xs (x:result)
+
 -- 5. Write a function that determines whether its input list is a palindrome.
--- isPalindrome :: [a] -> Bool
+isPalindrome :: [Int] -> Bool
+isPalindrome xs = listsEqual xs (listReverse xs)
+    where listsEqual [] [] = True
+          listsEqual [] _ = False
+          listsEqual _ [] = False
+          listsEqual (x:xs) (y:ys) = x == y && listsEqual xs ys
+
+-- 6. Create a function that sorts a list of lists based on the length of each sublist.
+sortByLength :: [[a]] -> [[a]]
+sortByLength xs = sortBy compareByLength xs
+    where compareByLength xs ys = compare (listLength xs) (listLength ys)
+
+
